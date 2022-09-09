e0ddf59e803780c71b948e3c0e30645490e6a92a
root <root@ip-172-31-35-60.ap-south-1.compute.internal>
2022-09-09 10:29:07 +0000

    Bad test case  update

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -30,7 +30,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
